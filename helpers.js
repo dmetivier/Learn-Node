ccde430c5bdcfd4f63e5192bee2c0b71fa803995
dmetivier <devinmetivier@gmail.com>
2017-07-18 18:24:02 -0400

    app

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 /*
-  This is a file of data and helper functions that we can expose and use in our templating function
-*/
+ This is a file of data and helper functions that we can expose and use in our templating function
+ */
 
 // FS is a built in module to node that let's us read files from the system we're running on
 const fs = require('fs');
@@ -18,12 +18,12 @@ exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/static
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-exports.siteName = 'Now That\'s Delicious!';
+exports.siteName = `Now That's Delicious!`;
 
 exports.menu = [
-  { slug: '/stores', title: 'Stores', icon: 'store', },
-  { slug: '/tags', title: 'Tags', icon: 'tag', },
-  { slug: '/top', title: 'Top', icon: 'top', },
-  { slug: '/add', title: 'Add', icon: 'add', },
-  { slug: '/map', title: 'Map', icon: 'map', },
+    { slug: '/stores', title: 'Stores', icon: 'store', },
+    { slug: '/tags', title: 'Tags', icon: 'tag', },
+    { slug: '/top', title: 'Top', icon: 'top', },
+    { slug: '/add', title: 'Add', icon: 'add', },
+    { slug: '/map', title: 'Map', icon: 'map', },
 ];
--- start.js
+++ start.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle an bad connections
-mongoose.connect(process.env.LOCAL);
-// mongoose.connect(process.env.DATABASE);
+// mongoose.connect(process.env.LOCAL);
+mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
