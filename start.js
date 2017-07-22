f0ecd0af588ec5a55883b356ccec769aed44d1d5
dmetivier <devinmetivier@gmail.com>
2017-07-22 00:45:46 -0400

    app complete

--- start.js
+++ start.js
@@ -8,7 +8,7 @@ require('dotenv').config({ path: 'variables.env' });
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
-  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
 // READY?! Let's go!
@@ -22,5 +22,5 @@ require('./models/Review');
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+    console.log(`Express running → PORT ${server.address().port}`);
 });
